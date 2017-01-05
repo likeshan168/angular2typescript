25428343d86c6bde49d28e3c95dd825757ab55e1
keshan li <keshan li>
2017-01-05 17:27:07 +0800

    创建登录界面

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>Hello {{name}}</h1>
-    <my-title [subtitle]="subtitle"></my-title>
-     <nav>
-      <a routerLink="contact" routerLinkActive="active">Contact</a>
-      <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
-      <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'components/app/app.component.html'
 })
 export class AppComponent {
-  name = 'Angular';
-  subtitle = '(v1)';
+  title:string = "个人主页"
 }
